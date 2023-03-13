d1c2bb80e5c16256c3011e790bda1d9f322227a9
antohaot <otanton2006@gmail.com>
2023-03-13 21:54:18 +0300

    HomeWork 2

--- 4.py
+++ 4.py
@@ -1,13 +1,33 @@
-n = int(input())
-b = 0
-max = 0
+a = int(input())
+b = 1
+c = 0
 
-while n > max:
-    if n >= max:
-        max = n
+while a > 0: 
+
+
+
+    
+    if a > c: 
+        c = a
         b += 1
-        n = int(input())
-    else:
-        b += 0
-        n = int(input())
+        a = int(input())
+
+    elif a == c:
+        c = a
+        b = 0
+        a = int(input())
+
+    elif a < c:
+        c = a
+        b += 1
+        a = int(input()) 
+
 print(b)
+
+
+#    1   2
+#    7   5
+#    7   8
+#    9   1
+#    1   3
+#    0   0
